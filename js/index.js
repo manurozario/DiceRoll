178819484ee3a7877f9a77aee4efee7a1e3e2376
manurozario <manurozario@users.noreply.github.com>
2020-07-07 15:17:44 -0500

    Updated Positioning

--- js/index.js
+++ js/index.js
@@ -3,53 +3,33 @@ function vsblty(target) {
   document.querySelector(target).classList.toggle("visibility");
 }
 
-//function to generate a random number
-function randomNum() {
-  var num = Math.floor(Math.random() * 6);
+//function to generate a random number then replace the dice image accordingly
+function randomNum(index) {
+  var num = Math.floor((Math.random() * 6)+1);
+  var dice = "images/dice" + num + ".png"
+  document.querySelectorAll("img")[index].setAttribute("src",dice);
   return num;
 }
 
 
-//function that runs the array in a loop so that the dice only represents the generated number
-function dices(num,plyr) {
-  for(var i = 0; i < plyr.length; i++) {
-    if(i === num) {
-      continue;
-    }
-    vsblty(plyr[i]);
-  }
-}
-
-
-//these tags are hidden by default
-vsblty("h1.player1");
-vsblty("h1.player2");
-vsblty("h1.draw");
-
-//arrays to refer to the images
-var plyr1 = [".plyr1 .dice1", ".plyr1 .dice2", ".plyr1 .dice3", ".plyr1 .dice4", ".plyr1 .dice5", ".plyr1 .dice6"];
-var plyr2 = [".plyr2 .dice1", ".plyr2 .dice2", ".plyr2 .dice3", ".plyr2 .dice4", ".plyr2 .dice5", ".plyr2 .dice6"];
-
 //make to two random numbers
-var num1 = randomNum();
-var num2 = randomNum();
+var num1 = randomNum(0);
+var num2 = randomNum(1);
 
-//views dice numbers representing those numbers
-dices(num1,plyr1);
-dices(num2,plyr2);
+
 
 //use if statements to show/hide messages based on what numbers are generated
 if(num1 > num2) {
-  vsblty("h1.player1");
-  vsblty("h1.header");
+  vsblty("h1.player2");
+  vsblty("h1.draw");
 }
 
 else if(num2 > num1) {
-  vsblty("h1.player2");
-  vsblty("h1.header");
+  vsblty("h1.player1");
+  vsblty("h1.draw");
 }
 
 else if(num1 === num2) {
-  vsblty("h1.draw");
-  vsblty("h1.header");
+  vsblty("h1.player1");
+  vsblty("h1.player2");
 }
